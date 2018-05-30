d99a8f68f4f9d83e80c249282f437a8c479e6e05
Willy Liu <arkazain@gmail.com>
2018-05-30 10:56:01 +0200

    🚧 Add initial post component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React, { Component } from "react";
+import authenticate from "./api/authentication";
+import Post from "./components/Post/";
 
 class App extends Component {
+  state = {
+    posts: [],
+  };
+
+  async componentDidMount() {
+    try {
+      const snoo = await authenticate();
+      const posts = await snoo.getHot();
+      console.log(posts);
+      this.setState({ posts });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   render() {
+    const { posts } = this.state;
     return (
-      <div>Hello</div>
+      <div className="post-feed">
+        {posts.length !== 0 &&
+          posts.map(post => <Post key={post.id} post={post} />)}
+      </div>
     );
   }
 }
---
+++ src/components/Post/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from "prop-types";
+import moment from "moment-mini";
+import "./Post.css";
+
+/**
+ * Component for a post in the feed
+ */
+function Post({ post }) {
+  return (
+    <div className="post-component">
+      <div className="post-score">{post.score}</div>
+      <div className="post-data">
+        <div className="post-title">{post.title}</div>
+        <div className="post-date">
+          {moment.unix(post.created_utc).fromNow()} by {post.author.name}
+        </div>
+        <div className="post-sub">{post.subreddit.display_name}</div>
+      </div>
+    </div>
+  );
+}
+
+Post.propTypes = {
+  post: PropTypes.object.isRequired,
+};
+
+export default Post;
