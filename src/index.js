bb2baae48e26c4455441801eb4c878b1d21ab46e
Willy Liu <arkazain@gmail.com>
2018-05-31 08:43:26 +0200

    ⚙️ Install fontawesome-free-brands

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import fontawesome from "@fortawesome/fontawesome";
 import faSolid from "@fortawesome/fontawesome-pro-solid";
+import faBrands from "@fortawesome/fontawesome-free-brands";
 import "normalize.css";
 
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import "./index.scss";
 
-fontawesome.library.add(faSolid);
+fontawesome.library.add(faSolid, faBrands);
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
