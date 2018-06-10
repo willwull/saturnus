6df79d18fee84fe1734ab0e558785baeca603b83
Willy Liu <arkazain@gmail.com>
2018-06-10 14:09:36 +0200

    🚧 Remove unnecessary import in Dropdown

--- src/components/Dropdown/index.js
+++ src/components/Dropdown/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import FaIcon from "@fortawesome/react-fontawesome";
 import "./Dropdown.scss";
 
 class Dropdown extends Component {
