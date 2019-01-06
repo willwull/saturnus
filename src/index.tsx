2b7453f55a195d270bb3d25f3d8b7be61daad36a
Willy Liu <arkazain@gmail.com>
2019-01-06 22:01:37 +0100

    ⚙️ Install intersection-observer polyfill

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,7 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { faSafari } from "@fortawesome/free-brands-svg-icons";
 import { fas } from "@fortawesome/pro-solid-svg-icons";
 import { far } from "@fortawesome/pro-regular-svg-icons";
+import "intersection-observer"; // mostly for Safari
 import "normalize.css";
 
 import reducer from "./reducers";
