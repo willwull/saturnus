640a8d20f02c0396846222c32d519fd9e1b2e1a3
Willy Liu <arkazain@gmail.com>
2019-01-16 15:19:13 +0100

    🚧 Add page for search

---
+++ src/components/Page.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const Page = styled.div`
+  max-width: 850px;
+  margin: 70px auto 20px auto;
+`;
+
+export default Page;
---
+++ src/pages/Searchpage.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { RouteComponentProps, Redirect } from "react-router-dom";
+import Page from "../components/Page";
+
+type Props = RouteComponentProps;
+
+function Searchpage({ location }: Props) {
+  const queryString = location.search;
+  const searchParams = new URLSearchParams(queryString);
+  const q = searchParams.get("q");
+
+  if (!q) {
+    // if search query is empty, go home instead
+    return <Redirect to="/" />;
+  }
+
+  return <Page>Search results for {q}</Page>;
+}
+
+export default Searchpage;
