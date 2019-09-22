cc3abd9a67bc56f6f44fe06f8851906d29493cf2
Willy Liu <willyliu@fb.com>
2019-09-22 22:00:01 +0100

    🌈 Update post more button position, change saved appearance

--- src/components/Post/styles.tsx
+++ src/components/Post/styles.tsx
@@ -1,6 +1,19 @@
 import styled from "styled-components";
 import Color from "color";
 import { Link } from "react-router-dom";
+import ContentBox from "../ContentBox";
+import { ThemeColors } from "../../reducers/theme";
+
+type ContainerProps = {
+  saved: boolean;
+  theme: ThemeColors;
+};
+
+export const Container = styled(ContentBox)`
+  border-bottom: 5px solid;
+  border-color: ${(p: ContainerProps) =>
+    p.saved ? p.theme.mod : p.theme.contentBg};
+`;
 
 // This component is used to make the entire post covered
 // by a Link component, so that the entire post can be
@@ -42,11 +55,7 @@ export const SubredditLink = styled(Link)`
 `;
 
 export const DropDownBtnWrapper = styled.div`
-  margin-left: auto;
-  margin-right: 14px;
-
   button {
-    font-size: 1.5em;
     width: 44px;
   }
 `;
@@ -94,9 +103,8 @@ export const Score = styled.div`
   margin: 5px 0;
 `;
 
-export const SavedIconWrapper = styled.div`
-  position: absolute;
-  top: 0;
-  right: 20px;
-  font-size: 1.5em;
+export const TitleAndMoreContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
