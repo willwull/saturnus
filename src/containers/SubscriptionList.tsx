f67eb6cae339690a3388062eac20345d9d3bf2b9
Willy Liu <willyjliu@gmail.com>
2020-05-12 21:15:11 +0200

    🐛 Fix subscriptions race conditions,  caching bug

--- src/containers/SubscriptionList.tsx
+++ src/containers/SubscriptionList.tsx
@@ -1,72 +1,36 @@
-import React, { Component } from "react";
-import { withRouter, RouteComponentProps } from "react-router-dom";
-import { connect } from "react-redux";
-import { fetchMySubs } from "../actions/user";
-import SubredditList, { SimpleSubreddit } from "../components/SubredditList";
-import Loading from "../components/Loading";
-import { RootState, DispatchType } from "../reducers";
+import React from "react";
+import { useSelector } from "react-redux";
 import styled from "styled-components";
-
-type StateProps = {
-  isLoading: boolean;
-  subscriptions: SimpleSubreddit[];
-  error: string;
-};
-
-type DispatchProps = {
-  fetch: () => void;
-};
-
-type Props = StateProps & DispatchProps & RouteComponentProps;
+import SubredditList from "../components/SubredditList";
+import Loading from "../components/Loading";
+import { RootState } from "../reducers";
 
 const ErrorMessage = styled.div`
-  padding: 15px 0;
+  padding: 15px;
 `;
 
-class SubscriptionList extends Component<Props, {}> {
-  componentDidMount() {
-    this.props.fetch();
-  }
-
-  render() {
-    const { isLoading, error, subscriptions } = this.props;
-
-    if (isLoading && subscriptions.length === 0) {
-      return <Loading type="regular" />;
-    }
-
-    if (error) {
-      return <ErrorMessage>{this.props.error}</ErrorMessage>;
-    }
-
-    return <SubredditList subreddits={subscriptions} />;
-  }
-}
-
-function mapStateToProps({
-  user: { subscriptions, subsLoading, subsError },
-}: RootState): StateProps {
-  return {
-    isLoading: subsLoading,
+function SubscriptionList() {
+  const userState = useSelector((state: RootState) => state.user);
+  const {
+    subsLoading,
+    isLoading: userLoading,
+    subsError: error,
     subscriptions,
-    error: subsError || "",
-  };
+  } = userState;
+
+  const isLoading = subsLoading || userLoading;
+
+  // If we're in the process of getting the user info, we display a loader here
+  // because the subscription fetch can't happen until the user info is fetched first.
+  if (isLoading && subscriptions.length === 0) {
+    return <Loading type="regular" />;
+  }
+
+  if (error) {
+    return <ErrorMessage>{error}</ErrorMessage>;
+  }
+
+  return <SubredditList subreddits={subscriptions} />;
 }
 
-function mapDispatchToProps(dispatch: DispatchType): DispatchProps {
-  return {
-    fetch: () => {
-      dispatch(fetchMySubs({ skipCache: false }));
-    },
-  };
-}
-
-// withRouter is necessary in order for this component to
-// update when the route changes, so the active links get
-// the active class
-export default withRouter(
-  connect(
-    mapStateToProps,
-    mapDispatchToProps,
-  )(SubscriptionList),
-);
+export default SubscriptionList;
