bb46364813e32176a8208e03280090444cb02b2a
Willy Liu <willyjliu@gmail.com>
2020-05-16 14:44:46 +0200

    ⚙️ Set up Eslint for TS

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,27 @@
 module.exports = {
-  extends: "airbnb", // for React
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "react", "react-hooks"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "prettier/@typescript-eslint",
+  ],
   env: {
-    node: true,
     browser: true,
   },
-  parser: "babel-eslint",
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
   rules: {
-    quotes: ["error", "double"],
-    "arrow-parens": ["error", "as-needed"],
     "no-console": 0,
     "no-plusplus": 0,
     "react/jsx-filename-extension": 0,
-    "react/forbid-prop-types": 0,
     "import/prefer-default-export": 0,
-    "function-paren-newline": 0,
     "jsx-a11y/media-has-caption": 0,
     "react/no-danger": 0,
     "react/sort-comp": 0,
@@ -22,21 +30,16 @@ module.exports = {
     "object-curly-newline": 0,
     "no-underscore-dangle": 0,
     "react/prefer-stateless-function": 0,
-    "arrow-body-style": 0,
     "prefer-destructuring": 0,
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
     "no-unused-expressions": [
       "error",
       {
         allowTaggedTemplates: true,
       },
     ],
-    "jsx-a11y/anchor-is-valid": [
-      "error",
-      {
-        components: ["Link"],
-        specialLink: ["to"],
-        aspects: ["noHref", "invalidHref", "preferButton"],
-      },
-    ],
+    "@typescript-eslint/explicit-function-return-type": 0,
+    "@typescript-eslint/no-explicit-any": 0,
   },
 };
